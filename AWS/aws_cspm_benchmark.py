17abce39d10fd82439a548af15ad9634ed911662
Simon Lukasik <simon.lukasik@crowdstrike.com>
2022-10-18 17:18:15 +0100

    Remove another mechanism to filter out regions

--- AWS/aws_cspm_benchmark.py
+++ AWS/aws_cspm_benchmark.py
@@ -27,8 +27,6 @@ def process(reg: str, svc: list) -> dict:
 
 aws_account = {}
 aws_account["totals"] = {}
-include = ["all"]
-# include = ["eu-west-2", "us-east-2", "us-west-1", "ap-southeast-2"]
 checks = [
     ["ec2", "Reservations", "describe_instances", "MaxResults", "1000", "ec2"],
     ["rds", "DBInstances", "describe_db_instances", "MaxRecords", "100", "rds"],
@@ -64,29 +62,29 @@ ec2 = boto3.client("ec2")
 response = ec2.describe_regions()
 for region in response["Regions"]:
     RegionName = region["RegionName"]
-    if RegionName in include or "all" in include:
-        # Setup the branch
-        print(f"Processing {RegionName}")
-        aws_account[RegionName] = {}
-        aws_account["totals"][RegionName] = {}
-        # Create the row for our output table
-        row = {}
-        row["region"] = RegionName
-        for service in checks:
-            # Process each service, adding the results to the aws_account object
-            aws_account[RegionName][service[5]] = process(RegionName, service)
-            # Calculate the number of elements found and throw it in the totals branch
-            aws_account["totals"][RegionName][service[5]] = len(aws_account[RegionName][service[5]][service[1]])
-            if service[5] not in ["iam"]:
-                # Increment the number of elements found for the service in our grand total
-                totals[service[5]] += aws_account["totals"][RegionName][service[5]]
-            else:
-                # IAM roles are not region-specific, so just overwrite the previous value (it'll be the same)
-                totals[service[5]] = aws_account["totals"][RegionName][service[5]]
-            # Update the row with this service's totals
-            row.update(aws_account["totals"][RegionName])
-        # Add the row to our display table
-        data.append(row)
+
+    # Setup the branch
+    print(f"Processing {RegionName}")
+    aws_account[RegionName] = {}
+    aws_account["totals"][RegionName] = {}
+    # Create the row for our output table
+    row = {}
+    row["region"] = RegionName
+    for service in checks:
+        # Process each service, adding the results to the aws_account object
+        aws_account[RegionName][service[5]] = process(RegionName, service)
+        # Calculate the number of elements found and throw it in the totals branch
+        aws_account["totals"][RegionName][service[5]] = len(aws_account[RegionName][service[5]][service[1]])
+        if service[5] not in ["iam"]:
+            # Increment the number of elements found for the service in our grand total
+            totals[service[5]] += aws_account["totals"][RegionName][service[5]]
+        else:
+            # IAM roles are not region-specific, so just overwrite the previous value (it'll be the same)
+            totals[service[5]] = aws_account["totals"][RegionName][service[5]]
+        # Update the row with this service's totals
+        row.update(aws_account["totals"][RegionName])
+    # Add the row to our display table
+    data.append(row)
 # Add in our grand totals to the display table
 data.append(totals)
 # Create GRAND_TOTAL_RESOURCE count for quoting
