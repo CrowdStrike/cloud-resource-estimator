60621dad588f298bdb0119058d700644a191d370
Simon Lukasik <simon.lukasik@crowdstrike.com>
2022-10-26 13:53:53 +0100

    Implement AWS Organization -> go through all AWS accounts in the ORG

--- AWS/aws_cspm_benchmark.py
+++ AWS/aws_cspm_benchmark.py
@@ -47,26 +47,71 @@ totals = {
             'kubenodes_running': 0
 }
 
+
 class AWSOrgAccess:
-    @classmethod
-    def accounts(cls):
+    def accounts(self):
         try:
             client = boto3.client('organizations')
             response = client.list_accounts()
-        except client.exceptions.AccessDeniedException as exc:
+            accounts = response['Accounts']
+            next_token = response.get('NextToken', None)
+
+            while next_token:
+                response = client.list_accounts(NextToken=next_token)
+                accounts += response['Accounts']
+                next_token = response.get('NextToken', None)
+
+            return [self.aws_handle(a) for a in accounts]
+        except client.exceptions.AccessDeniedException:
             print("Cannot autodiscover adjacent accounts: cannot list accounts within the AWS organization")
             return [AWSHandle()]
+
+    def aws_handle(self, account):
+        if account['Id'] == self.master_account_id:
+            return AWSHandle(aws_session=self.master_session)
+        return AWSHandle(aws_session=self.new_session(account['Id']))
+
+    def new_session(self, account_id):
+        try:
+            credentials = self.master_sts.assume_role(
+                RoleArn=f'arn:aws:iam::{account_id}:role/OrganizationAccountAccessRole',
+                RoleSessionName=account_id
+            )
+            return boto3.session.Session(
+                aws_access_key_id=credentials['Credentials']['AccessKeyId'],
+                aws_secret_access_key=credentials['Credentials']['SecretAccessKey'],
+                aws_session_token=credentials['Credentials']['SessionToken'],
+                region_name='us-east-1'
+               )
+        except self.master_sts.exceptions.ClientError as exc:
+            print("Cannot access adjacent account: ", account_id, exc)
+            raise exc
+
+    @cached_property
+    def master_session(self):
+        return boto3.session.Session()
+
+    @cached_property
+    def master_sts(self):
+        return self.master_session.client('sts')
+
+    @cached_property
+    def master_account_id(self):
+        return self.master_sts.get_caller_identity()["Account"]
 
 
 class AWSHandle:
     EKS_TAGS = ['eks:cluster-name', 'alpha.eksctl.io/nodegroup-type', 'aws:eks:cluster-name', 'eks:nodegroup-name']
+
+    def __init__(self, aws_session=None):
+        self.aws_session = aws_session if aws_session else boto3.session.Session()
 
     @property
     def regions(self):
         return self.ec2.describe_regions()['Regions']
 
     def ec2_instances(self, aws_region):
-        client = boto3.client('ec2', aws_region)
+        client = self.aws_session.client('ec2', aws_region)
 
         response = client.describe_instances(MaxResults=1000)
         instances = response['Reservations']
@@ -81,18 +126,18 @@ class AWSHandle:
 
     @cached_property
     def ec2(self):
-        return boto3.client("ec2")
+        return self.aws_session.client("ec2")
 
     @classmethod
-    def is_vm_kubenode(cls, instance):
-        return any(True for tag in instance.get('Tags', []) if tag['Key'] in cls.EKS_TAGS)
+    def is_vm_kubenode(cls, vm):
+        return any(True for tag in vm.get('Tags', []) if tag['Key'] in cls.EKS_TAGS)
 
     @classmethod
     def is_vm_running(cls, vm):
         return vm['State']['Name'] != 'stopped'
 
 
-for aws in AWSOrgAccess.accounts():
+for aws in AWSOrgAccess().accounts():
     for region in aws.regions:
         RegionName = region["RegionName"]
 
